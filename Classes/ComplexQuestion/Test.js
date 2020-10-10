33a1fed40a660373109219a4142b86a508d0bab8
theCodeHog <paolatosca@outlook.com>
2020-10-10 23:55:17 +0200

    Added Test Class

---
+++ Classes/ComplexQuestion/Test.js
@@ -0,0 +1,7 @@
+module.exports = class Test {
+  constructor(name, description, possibleResults) {
+    this.name = name;
+    this.description = description;
+    this.possibleResults = possibleResults;
+  }
+};
