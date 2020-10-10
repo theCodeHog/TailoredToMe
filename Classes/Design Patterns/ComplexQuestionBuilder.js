fcef3264040e78db83b0ef64a1144a06c6ec86fb
theCodeHog <paolatosca@outlook.com>
2020-10-11 00:01:18 +0200

    Implemented Builder Pattern for both ComplexQuestion and Test

---
+++ Classes/Design Patterns/ComplexQuestionBuilder.js
@@ -0,0 +1,59 @@
+const ComplexQuestion = require("../ComplexQuestions/ComplexQuestion");
+const QuestionFactory = require("./QuestionFactory");
+const TestBuilder = require("../Design Patterns/TestBuilder");
+
+module.exports = class ComplexQuestionBuilder {
+  constructor() {
+    this.questionProperties = {};
+  }
+
+  setName(name) {
+    this.questionProperties.name = name;
+    return this;
+  }
+
+  setColor(color) {
+    this.questionProperties.color = color;
+    return this;
+  }
+
+  setCategory(category) {
+    this.questionProperties.category = category;
+    return this;
+  }
+
+  setQuestions(questions) {
+    this.questionProperties.questions = [];
+    questions.map((question) => {
+      this.questionProperties.questions.push(
+        QuestionFactory.createQuestion(question.name, question.answers)
+      );
+    });
+    return this;
+  }
+
+  setTests(tests) {
+    this.questionProperties.tests = [];
+    tests.map((test) => {
+      this.questionProperties.tests.push(
+        new TestBuilder()
+          .setName(test.name)
+          .setDescription(test.description)
+          .setPossibleResults(test.possibleResults)
+          .createTest()
+      );
+    });
+    return this;
+  }
+
+  createComplexQuestion() {
+    let p = this.questionProperties;
+    return new ComplexQuestion(
+      p.name,
+      p.color,
+      p.category,
+      p.questions,
+      p.tests
+    );
+  }
+};
---
+++ Classes/Design Patterns/TestBuilder.js
@@ -0,0 +1,27 @@
+const Test = require("../ComplexQuestions/Test");
+
+module.exports = class TestBuilder {
+  constructor() {
+    this.testProperties = {};
+  }
+
+  setName(name) {
+    this.testProperties.name = name;
+    return this;
+  }
+
+  setDescription(description) {
+    this.testProperties.description = description;
+    return this;
+  }
+
+  setPossibleResults(possibleResults) {
+    this.testProperties.possibleResults = possibleResults;
+    return this;
+  }
+
+  createTest() {
+    let p = this.testProperties;
+    return new Test(p.name, p.description, p.possibleResults);
+  }
+};
